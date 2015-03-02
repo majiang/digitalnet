aa1d871eaff90b1dd4e062f70cbf4e26b9a7c4d4
majiang <neuron.ohori@gmail.com>
2015-03-02 19:27:05 +0900

    Separate integral to bisectable and non-bisectable cases

--- source/digitalnet/integration.d
+++ source/digitalnet/integration.d
@@ -6,15 +6,38 @@ import std.range : ElementType;
 import std.typecons : Flag;
 alias Centering = Flag!"centering";
 
-auto integral(Centering centering=Centering.yes, S, Function)(S P, Function f)
+auto _integral(Centering centering=Centering.yes, S, Function)(S P, Function f)
 	if (isPointSet!S && hasPrecision!S && hasDimensionR!S &&
 		isArray!(ParameterTypeTuple!Function) && isFloatingPoint!(ElementType!(ParameterTypeTuple!Function)))
 {
+	pragma(msg, "_integral!(", typeid(S), ", ", typeid(Function), ")");
 	alias F = ElementType!(ParameterTypeTuple!Function);
 	F result = 0;
 	foreach (x; P.toReals!(F, centering, S))
 		result += f(x);
 	return result * exp2(-cast(ptrdiff_t)P.dimensionF2);
+}
+
+auto integral(Centering centering=Centering.yes, S, Function)(S P, Function f)
+	if (isPointSet!S && hasPrecision!S && hasDimensionR!S
+	 && isArray!(ParameterTypeTuple!Function) && isFloatingPoint!(ElementType!(ParameterTypeTuple!Function))
+	 && !(isBisectable!S))
+{
+	pragma(msg, "integralNonbisectable!(", typeid(S), ", ", typeid(Function), ")");
+	return _integral!(centering, S, Function)(P, f);
+}
+
+auto integral(Centering centering=Centering.yes, S, Function)(S P, Function f)
+	if (isPointSet!S && hasPrecision!S && hasDimensionR!S
+	 && isArray!(ParameterTypeTuple!Function) && isFloatingPoint!(ElementType!(ParameterTypeTuple!Function))
+	 && (isBisectable!S))
+{
+	pragma(msg, "integralBisectable!(", typeid(S), ", ", typeid(Function), ")");
+	if (!P.bisectable)
+		return _integral!(centering, S, Function)(P, f);
+	auto Q = P.bisect;
+	return (integral!(centering, S, Function)(Q[0], f) +
+	        integral!(centering, S, Function)(Q[1], f)) / 2;
 }
 
 auto toReals(F, Centering centering=Centering.yes, S)(S P)
