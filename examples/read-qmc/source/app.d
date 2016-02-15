e434b63a0fb91d85722b593047a9a1ecd2b271dc
majiang <neuron.ohori@gmail.com>
2016-02-15 21:27:45 +0900

    Update an example

--- examples/read-qmc/source/app.d
+++ examples/read-qmc/source/app.d
@@ -1,12 +1,19 @@
-import std.stdio, std.conv, std.algorithm;
+import std.stdio, std.conv, std.algorithm, std.array, std.string, std.math;
 import digitalnet.implementation;
 import digitalnet.integration;
 
 void main(string[] args)
 {
+	auto cs = (args.length == 1) ? [real(1)] :
+	(){
+		return args[1..$].map!(to!real).array;
+	}();
 	foreach (line; stdin.byLine)
 	{
-		auto P = line.toShiftedDigitalNet!uint;
-		writefln("%s,%.15e", P, P.integral((real[] x) => x.reduce!min));
+		write(line.chomp);
+		auto P = line.toDigitalNet!uint;
+		foreach (c; cs)
+			writef(",%.15e", P.integral((real[] x) => (x.reduce!((a, b) => a + b) * c).cos));
+		writeln;
 	}
 }
